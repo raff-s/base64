cff0f629820814b1179808a56586506ca62b6656
raff <r4faelsimoes@gmail.com>
2025-02-01 15:42:06 +1100

    decode

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Base64
+
+[Base64 encoding](https://datatracker.ietf.org/doc/html/rfc4648#section-4)
--- src/main.zig
+++ src/main.zig
@@ -11,8 +11,56 @@ const Base64 = struct {
         return Base64{ ._table = upper ++ lower ++ numbers_symb };
     }
 
-    pub fn _char_at(self: Base64, index: u8) u8 {
+    fn _char_at(self: Base64, index: u8) u8 {
         return self._table[index];
+    }
+
+    // This is not efficient, but it works
+    // we could use a hash table
+    // or use a binary search
+    fn _char_index(self: Base64, char: u8) u8 {
+        if (char == '=') {
+            return 64;
+        }
+
+        var index: u8 = 0;
+
+        while (index < 64) {
+            if (self._table[index] == char) {
+                return index;
+            }
+            index += 1;
+        }
+        unreachable;
+    }
+
+    fn decode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
+        if (input.len == 0) {
+            return "";
+        }
+        const n_output = try _calc_decode_len(input);
+        var output = try allocator.alloc(u8, n_output);
+        var count: u8 = 0;
+        var iout: u64 = 0;
+        var buf = [4]u8{ 0, 0, 0, 0 };
+
+        for (0..input.len) |i| {
+            buf[count] = self._char_index(input[i]);
+            count += 1;
+            if (count == 4) {
+                output[iout] = (buf[0] << 2) + (buf[1] >> 4);
+                if (buf[2] != 64) {
+                    output[iout + 1] = (buf[1] << 4) + (buf[2] >> 2);
+                }
+                if (buf[3] != 64) {
+                    output[iout + 2] = (buf[2] << 6) + buf[3];
+                }
+                iout += 3;
+                count = 0;
+            }
+        }
+
+        return output;
     }
 
     pub fn encode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
@@ -79,14 +127,15 @@ fn _calc_encode_len(input: []const u8) !usize {
     return n_output * 4;
 }
 
-fn _calc_decoded_len(input: []const u8) !usize {
-    if (input.len < 4) {
-        return 3;
+fn _calc_decode_len(input: []const u8) !usize {
+    var padding: usize = 0;
+    if (input.len >= 1 and input[input.len - 1] == '=') {
+        padding += 1;
     }
-
-    const n_output = try std.math.divFloor(usize, input.len, 4);
-
-    return n_output * 3;
+    if (input.len >= 2 and input[input.len - 2] == '=') {
+        padding += 1;
+    }
+    return (input.len / 4) * 3 - padding;
 }
 
 pub fn main() !void {}
@@ -102,18 +151,15 @@ test "base64._char_at" {
     try std.testing.expectEqual(base64._char_at(63), '/');
 }
 
-test "base64._calc_encoded_len" {
-    try std.testing.expectEqual(try _calc_encode_len(""), 4);
-    try std.testing.expectEqual(try _calc_encode_len("ab"), 4);
-    try std.testing.expectEqual(try _calc_encode_len("abc"), 4);
-    try std.testing.expectEqual(try _calc_encode_len("abcd"), 8);
-}
-
-test "base64._calc_decoded_len" {
-    try std.testing.expectEqual(try _calc_decoded_len(""), 3);
-    try std.testing.expectEqual(try _calc_decoded_len("aa"), 3);
-    try std.testing.expectEqual(try _calc_decoded_len("aaa"), 3);
-    try std.testing.expectEqual(try _calc_decoded_len("aaaaaaaa"), 6);
+test "base64._char_index" {
+    const base64 = Base64.init();
+    try std.testing.expectEqual(base64._char_index('A'), 0);
+    try std.testing.expectEqual(base64._char_index('Z'), 25);
+    try std.testing.expectEqual(base64._char_index('a'), 26);
+    try std.testing.expectEqual(base64._char_index('z'), 51);
+    try std.testing.expectEqual(base64._char_index('0'), 52);
+    try std.testing.expectEqual(base64._char_index('+'), 62);
+    try std.testing.expectEqual(base64._char_index('='), 64);
 }
 
 test "base64.encode" {
@@ -127,4 +173,26 @@ test "base64.encode" {
     const output2 = try base64.encode(std.testing.allocator, input2);
     defer std.testing.allocator.free(output2);
     try std.testing.expectEqualSlices(u8, output2, "MA==");
+}
+
+test "base64.decode" {
+    const base64 = Base64.init();
+    const input = "aGVsbG8gd29ybGQ=";
+    const output = try base64.decode(std.testing.allocator, input);
+    defer std.testing.allocator.free(output);
+    try std.testing.expectEqualSlices(u8, output, "hello world");
+}
+
+test "_calc_encode_len" {
+    try std.testing.expectEqual(try _calc_encode_len(""), 4);
+    try std.testing.expectEqual(try _calc_encode_len("ab"), 4);
+    try std.testing.expectEqual(try _calc_encode_len("abc"), 4);
+    try std.testing.expectEqual(try _calc_encode_len("abcd"), 8);
+}
+
+test "_calc_decode_len" {
+    try std.testing.expectEqual(try _calc_decode_len(""), 0);
+    try std.testing.expectEqual(try _calc_decode_len("aa"), 0);
+    try std.testing.expectEqual(try _calc_decode_len("aaa"), 0);
+    try std.testing.expectEqual(try _calc_decode_len("aaaaaaaa"), 6);
 }
